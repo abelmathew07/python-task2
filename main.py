35b8d104e11448a89b7a68bb6d8ed0bed897cbb8
Abel Mathew Simon <abelmathewyo22@gmail.com>
2023-06-28 12:50:21 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+from StudentClass import Student
+
+# create a new student instance
+student1 = Student("TINO.S N", "TVM", 19)
+
+# display student details
+student1.display_student_details()
+
+# set and get age
+student1.set_age(21)
+print(student1.get_age())
